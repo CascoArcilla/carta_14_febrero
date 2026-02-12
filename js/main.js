4483016ab87be4586e5c567488517477a89b96e1
CascoArcilla <normon1230@gmail.com>
2026-02-11 22:12:12 -0600

    moviendo repo

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $(".valentines-day").click(function () {
+    $(".envelope").css({
+      animation: "fall 3s linear 1",
+      "-webkit-animation": "fall 3s linear 1",
+    });
+
+    $(".envelope").fadeOut(800, function () {
+      $(".valentines-day").hide();
+
+      $("#card").css({
+        display: "flex",
+        opacity: 0,
+        transform: "scale(0.1)",
+      });
+
+      $("#card").animate(
+        { opacity: 1 },
+        {
+          duration: 1000,
+          step: function (now, fx) {
+            var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+            $(this).css("transform", "scale(" + scale + ")");
+          },
+        },
+      );
+    });
+  });
+});
